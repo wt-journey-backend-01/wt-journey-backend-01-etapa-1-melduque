ec14f9ebb3b5907d01c6e3bc5c6d3c04b94a6f1e
Melissa Duque Baía <116390230+melduque@users.noreply.github.com>
2025-07-11 14:05:20 -0300

    Edição pasta

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app = express();
 const PORT = 3000;
 const path = require('path');
 
-/*app.get('/', (req, res) => {
-    res.send("Hello World!");
-});*/
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send("Bem-vindo ao servidor Express.js!");
+});
+
+app.post('/login', (req, res) => {
+    res.send(req.body);
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor da DevBurger rodando em localhost:${PORT}`);
